102fde8f0a69e60f5c0ffe50cb04dcd2aedce46b
gloverd <glover.d@gmail.com>
2015-02-17 10:40:19 -0500

    Update scripts.js

--- aqua-btr/js/scripts.js
+++ aqua-btr/js/scripts.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
         slidesToShow: 2,
         arrows: true
     });
+    
+});
+
+$('.create_slick').on('click', function() {
+      $('.center').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 2,
+        arrows: true
+    });
+
+});
+
+$('.destroy_slick').on('click', function() {
+
+     $('.center').slick('unslick');
 });
