3a3842f20a1b9df4e118fdc3152a74d9806ea54f
gloverd <glover.d@gmail.com>
2015-02-19 13:23:38 -0500

    Update functions.js

--- aqua-btr/trunk/js/functions.js
+++ aqua-btr/trunk/js/functions.js
@@ -1,53 +1,20 @@
-(function($){
- 
-    $.fn.shuffle = function() {
- 
-        var allElems = this.get(),
-            getRandom = function(max) {
-                return Math.floor(Math.random() * max);
-            },
-            shuffled = $.map(allElems, function(){
-                var random = getRandom(allElems.length),
-                    randEl = $(allElems[random]).clone(true)[0];
-                allElems.splice(random, 1);
-                return randEl;
-           });
- 
-        this.each(function(i){
-            $(this).replaceWith($(shuffled[i]));
-        });
- 
-        return $(shuffled);
- 
-    };
- 
-})(jQuery);
-
-
-
-jQuery('.slider').slick({
-    centerMode: true,
-    centerPadding: '100px',
-    dots: true
-});
-
-jQuery('.centerModeBtn').click(function() {
-    jQuery('.slider').slickSetOption('centerMode', false); 
-    jQuery('.buttons').append('<p>centerMode clicked</p>');  
-});
-jQuery('.addCenterModeBtn').click(function() {
-    jQuery('.slider').slickSetOption('centerMode', True); 
-    jQuery('.buttons').append('<p>centerMode clicked</p>');  
-});
-jQuery('.unslickBtn').click(function() {
-    jQuery('.slider').unslick();
-    jQuery('.buttons').append('<p>unslick clicked</p>');
-});
-jQuery('.slickBtn').click(function() {
-    jQuery('.slider').slick({
-    centerMode: true,
-    centerPadding: '100px',
-    dots: true
-});
-    jQuery('.buttons').append('<p>slick clicked</p>');
+$(document).ready(function(){
+    $(".btn-shuffle").bind('click', shuffle);        
+    function shuffle(){
+        $(".row").each(function(){
+            var divs = $(this).find('div');
+            for(var i = 0; i < divs.length; i++) $(divs[i]).remove();            
+            //the fisher yates algorithm, from http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
+            var i = divs.length;
+            if ( i == 0 ) return false;
+            while ( --i ) {
+               var j = Math.floor( Math.random() * ( i + 1 ) );
+               var tempi = divs[i];
+               var tempj = divs[j];
+               divs[i] = tempj;
+               divs[j] = tempi;
+             }
+            for(var i = 0; i < divs.length; i++) $(divs[i]).appendTo(this);
+        });                    
+    }
 });
